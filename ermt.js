a9b1b3042f29806a1b9e95d5fe4bde1800cf9af0
cgcostume <cgdaniel@outlook.com>
2019-06-13 11:46:56 +0200

    webgl1 and edge compatibility

--- ermt.js
+++ ermt.js
@@ -1,204 +1,30 @@
 
-
 class Renderer extends gloperate.Renderer {
 
-    SHADER_SOURCE_VERT =
-`precision highp float;
+    constructor() {
+        super();
 
-#if __VERSION__ == 100
-    attribute vec2 a_vertex;
-#else
-    in vec2 a_vertex;
-    #define varying out
-#endif
+        this._ndcRectangle = null;
 
-uniform vec2 u_ndcOffset;
-uniform ivec2 u_mode;
+        this._program = null;
+        this._uMode = null;
 
-varying vec2 v_uv;
-varying vec4 v_ray;
+        this._ndcOffsetKernel = null;
+        this._uNdcOffset = null;
 
-void main(void)
-{
-    vec2 v = a_vertex;// + u_ndcOffset;
-    vec3 rays[18] = vec3[](
-        vec3(  1.0, -v.y, -v.x), // px
-        vec3( -1.0, -v.y,  v.x), // nx
-        vec3(  v.x,  1.0,  v.y), // py
-        vec3(  v.x, -1.0, -v.y), // ny
-        vec3(  v.x, -v.y,  1.0), // pz
-        vec3( -v.x, -v.y, -1.0), // nz
-    
-        vec3(  1.0,  0.0,  0.0), // px
-        vec3( -1.0,  0.0,  0.0), // nx
-        vec3(  0.0,  1.0,  0.0), // py
-        vec3(  0.0, -1.0,  0.0), // ny
-        vec3(  0.0,  0.0,  1.0), // pz
-        vec3(  0.0,  0.0, -1.0), // nz
-    
-        vec3(  1.0,  0.0,  0.0), // px
-        vec3( -1.0,  0.0,  0.0), // nx
-        vec3(  0.0,  1.0,  0.0), // py
-        vec3(  0.0, -1.0,  0.0), // ny
-        vec3(  0.0,  0.0,  1.0), // pz
-        vec3(  0.0,  0.0, -1.0)  // nz
-    );
-    
-    v_uv = a_vertex;
-    v_ray = vec4(rays[u_mode[0]], 0.0);
-    
-    gl_Position = vec4(a_vertex.xy + u_ndcOffset, 0.0, 1.0);
-}
-`;
+        this._preview = null;
 
-    SHADER_SOURCE_FRAG =
-`precision highp float;
-precision highp int;
+        this._sourceTexture = null;
+        this._debug = true;
 
-#if __VERSION__ == 100
-    #define fragColor gl_FragColor
-#else
-    layout(location = 0) out vec4 fragColor;
-    #define varying in
-#endif
+        this._targetTexture = null;
+        this._targetFBO = null;
 
-uniform ivec2 u_mode;
+        this._queue = new Array();
+        this._task = null;
 
-uniform sampler2D u_equirectmap;
-
-varying vec2 v_uv;
-varying vec4 v_ray;
-
-const float PI         = 3.1415926535897932384626433832795;
-const float OneOver2PI = 0.1591549430918953357688837633725;
-const float OneOverPI  = 0.3183098861837906715377675267450;
-
-
-vec3 cubeTransform(in vec2 uv, in vec3 ray, in int mode) {
-    return ray;
-}
-
-
-vec3 sphereTransform(in vec2 uv, in vec3 ray, in int mode) {
-
-    float zz = 1.0 - dot(uv, uv);
-    // if(zz < 0.0) return -ray;
-
-    if(mode == 0) 
-        return reflect(-ray, vec3(  sqrt(zz), -uv.y, -uv.x)); // px
-    if(mode == 1) return reflect(-ray, vec3( -sqrt(zz), -uv.y,  uv.x)); // nx
-    if(mode == 2) return reflect(-ray, vec3(  uv.x,  sqrt(zz),  uv.y)); // py
-    if(mode == 3) return reflect(-ray, vec3(  uv.x, -sqrt(zz), -uv.y)); // ny
-    if(mode == 4) return reflect(-ray, vec3(  uv.x, -uv.y,  sqrt(zz))); // pz
-                  return reflect(-ray, vec3( -uv.x, -uv.y, -sqrt(zz))); // nz
-}
-
-
-vec3 paraboloidTransform(in vec2 uv, in vec3 ray, in int mode) {
-
-    vec3 foo;
-
-         if(mode == 0) foo = vec3(  1.0, -uv.y, -uv.x); // px
-    else if(mode == 1) foo = vec3( -1.0, -uv.y,  uv.x); // nx
-    else if(mode == 2) foo = vec3(  uv.x,  1.0,  uv.y); // py
-    else if(mode == 3) foo = vec3(  uv.x, -1.0, -uv.y); // ny
-    else if(mode == 4) foo = vec3(  uv.x, -uv.y,  1.0); // pz
-    else if(mode == 5) foo = vec3( -uv.x, -uv.y, -1.0); // nz
-
-    return reflect(-ray, normalize(foo));
-}
-
-// float catmulrom(float x) {
-//     float B = 0.0;
-//     float C = 0.5;
-//     float f = x;
-
-//     if(f < 0.0) {
-//         f = -f;
-//     }
-
-//     if(f < 1.0) {
-//         return ((+12.0 -  9.0 * B - 6.0 * C) * (f * f * f) 
-//               + (-18.0 + 12.0 * B + 6.0 * C) * (f * f) 
-//               + (+ 6.0 -  2.0 * B)) / 6.0;
-//     }
-
-//     if (f > 1.0 && f < 2.0) {
-//         return ((- 1.0 * B -  6.0 * C) * (f * f * f) 
-//               + (+ 6.0 * B + 30.0 * C) * (f * f) 
-//               + (-12.0 * B - 48.0 * C) * f
-//               + (+ 8.0 * B + 24.0 * C)) / 6.0;
-//     }
-//     return 0.0;
-// }
-
-// vec4 sharpen(in sampler2D sampler, in vec2 uv) {
-
-//     vec2 size = 1.0 / vec2(textureSize(sampler, 0));
-//     vec4 result = vec4(0.0);
-//     float weights = 0.0;
-
-//     for(int y = -4; y < 5; ++y)
-//         for(int x = -4; x < 5; ++x) {
-//             float f = sqrt(float(x) * float(x) + float(y) * float(y));
-//             float w = catmulrom(f);
-//             result += w * texture(sampler, uv + vec2(float(x), float(y)) * size);
-//             weights += w;
-//         }
-
-//     return result / weights;
-// }
-
-
-void main(void)
-{
-    vec3 ray = normalize(v_ray.xyz);
-
-    if(u_mode[0] < 6) {
-        ray = cubeTransform(v_uv, ray, u_mode[0]);
-    } else if (u_mode[0] < 12) {
-        ray = sphereTransform(v_uv, ray, u_mode[0] - 6);
-    } else if (u_mode[0] < 18) {
-        ray = paraboloidTransform(v_uv, ray, u_mode[0] - 12);
+        this._accumulate = null;
     }
-
-    if(u_mode[1] == 1) {
-        fragColor = vec4(ray * 0.5 + 0.5, 1.0);
-        return;
-    }
-
-    float v = acos(ray.y) * OneOverPI;
-    float m = atan(ray.x, ray.z);
-
-    vec2 uv = vec2(m * OneOver2PI + 0.5, v);
-    fragColor = texture(u_equirectmap, uv);
-    // fragColor = sharpen(u_equirectmap, uv);
-}
-`;
-    controller = null;
-
-
-    _ndcRectangle = null;
-
-    _program = null;
-    _uMode = null;
-    
-    _ndcOffsetKernel = null;
-    _uNdcOffset = null;
-    
-    _preview = null;
-
-    _sourceTexture = null;
-    _debug = true;
-
-    _targetTexture = null;
-    _targetFBO = null;
-
-    _queue = new Array();
-    _task = null;
-
-    _accumulate = null;
-
 
     onInitialize(context, callback, mouseEventProvider) {
 
@@ -244,14 +70,13 @@ void main(void)
         this._uNdcOffset = this._program.uniform('u_ndcOffset');
         this._uMode = this._program.uniform('u_mode');
 
-
+        
         this._ndcRectangle.bind();
 
 
         this._accumulate = new gloperate.AccumulatePass(this._context);
         this._accumulate.initialize();
         this._accumulate.precision = gloperate.Wizard.Precision.byte;
-
 
         return true;
     }
@@ -379,3 +204,157 @@ void main(void)
 
 }
 
+
+Renderer.prototype.SHADER_SOURCE_VERT = `
+precision highp float;
+precision highp int;
+
+#if __VERSION__ == 100
+    attribute vec2 a_vertex;
+#else
+    in vec2 a_vertex;
+    #define varying out
+#endif
+
+uniform vec2 u_ndcOffset;
+
+varying vec2 v_uv;
+
+void main(void)
+{
+    vec2 v = a_vertex;
+    v_uv = a_vertex;
+    
+    gl_Position = vec4(a_vertex.xy + u_ndcOffset, 0.0, 1.0);
+}
+`;
+
+
+Renderer.prototype.SHADER_SOURCE_FRAG = `
+precision highp float;
+precision highp int;
+
+#if __VERSION__ == 100
+    #define fragColor gl_FragColor
+    #define texture texture2D
+#else
+    layout(location = 0) out vec4 fragColor;
+    #define varying in
+#endif
+
+uniform ivec2 u_mode;
+
+uniform sampler2D u_equirectmap;
+
+varying vec2 v_uv;
+
+const float PI         = 3.1415926535897932384626433832795;
+const float OneOver2PI = 0.1591549430918953357688837633725;
+const float OneOverPI  = 0.3183098861837906715377675267450;
+
+const vec3 o = vec3(0.0, 1.0,-1.0); // orientation transform helper
+
+vec3 ray(in vec3 ray, in int mode) {
+
+         if(mode == 0) return ray.zyx * o.yzz; // px
+    else if(mode == 1) return ray.zyx * o.zzy; // nx
+    else if(mode == 2) return ray.xzy * o.yyy; // py
+    else if(mode == 3) return ray.xzy * o.yzz; // ny
+    else if(mode == 4) return ray.xyz * o.yzy; // pz
+                       return ray.xyz * o.zzz; // nz    
+}
+
+vec3 incident(in vec2 uv, in int mode) {
+
+    vec3 i = vec3(0.0, 1.0,-1.0);
+    
+         if(mode == 0) return i.zxx; // px
+    else if(mode == 1) return i.yxx; // nx
+    else if(mode == 2) return i.xzx; // py
+    else if(mode == 3) return i.xyx; // ny
+    else if(mode == 4) return i.xxz; // pz
+                       return i.xxy; // nz
+}
+
+vec3 cubeTransform(in vec2 uv, in int mode) {
+    return ray(normalize(vec3(uv, 1.0)), mode);    
+}
+
+vec3 sphereTransform(in vec2 uv, in int mode) {
+    vec3 i = incident(uv, mode);
+    vec3 r = ray(vec3(uv, sqrt(1.0 - dot(uv, uv))), mode);
+    return reflect(i, r);
+}
+
+vec3 paraboloidTransform(in vec2 uv, in int mode) {
+    vec3 i = incident(uv, mode);
+    vec3 r = ray(normalize(vec3(uv, 1.0)), mode);
+    return reflect(i, r);
+}
+
+void main(void)
+{
+    vec3 ray;
+
+    if(u_mode[0] < 6) {
+        ray = cubeTransform(v_uv, u_mode[0]);
+    } else if (u_mode[0] < 12) {
+        ray = sphereTransform(v_uv, u_mode[0] - 6);
+    } else if (u_mode[0] < 18) {
+        ray = paraboloidTransform(v_uv, u_mode[0] - 12);
+    }
+
+    if(u_mode[1] == 1) {
+        fragColor = vec4(ray * 0.5 + 0.5, 1.0);
+        return;
+    }
+
+    float v = acos(ray.y) * OneOverPI;
+    float m = atan(ray.x, ray.z);
+
+    vec2 uv = vec2(m * OneOver2PI + 0.5, v);
+    fragColor = texture(u_equirectmap, uv);
+}
+`;
+
+
+// float catmulrom(float x) {
+//     float B = 0.0;
+//     float C = 0.5;
+//     float f = x;
+
+//     if(f < 0.0) {
+//         f = -f;
+//     }
+
+//     if(f < 1.0) {
+//         return ((+12.0 -  9.0 * B - 6.0 * C) * (f * f * f) 
+//               + (-18.0 + 12.0 * B + 6.0 * C) * (f * f) 
+//               + (+ 6.0 -  2.0 * B)) / 6.0;
+//     }
+
+//     if (f > 1.0 && f < 2.0) {
+//         return ((- 1.0 * B -  6.0 * C) * (f * f * f) 
+//               + (+ 6.0 * B + 30.0 * C) * (f * f) 
+//               + (-12.0 * B - 48.0 * C) * f
+//               + (+ 8.0 * B + 24.0 * C)) / 6.0;
+//     }
+//     return 0.0;
+// }
+
+// vec4 sharpen(in sampler2D sampler, in vec2 uv) {
+
+//     vec2 size = 1.0 / vec2(textureSize(sampler, 0));
+//     vec4 result = vec4(0.0);
+//     float weights = 0.0;
+
+//     for(int y = -4; y < 5; ++y)
+//         for(int x = -4; x < 5; ++x) {
+//             float f = sqrt(float(x) * float(x) + float(y) * float(y));
+//             float w = catmulrom(f);
+//             result += w * texture(sampler, uv + vec2(float(x), float(y)) * size);
+//             weights += w;
+//         }
+
+//     return result / weights;
+// }
